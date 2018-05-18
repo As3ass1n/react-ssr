7531eb1f5b0198da2e571dbf1be7714ab452d42d
wxp <811273998@qq.com>
2018-05-18 22:57:22 +0800

    修改配置，编译生成server文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,44 @@
 const path = require('path');
 
-module.exports = {
-  entry: './app/app.js',
+const browserConfig = {
+  entry: './src/client/app.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/client'),
     filename: 'bundle.js',
   },
+  devtool: 'cheap-module-source-map',
   module: {
     rules: [
       {
+        test: /js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          query: { presets: ['react', 'env'] },
         },
       },
     ],
   },
 };
+
+const serverConfig = {
+  entry: './src/server/index.js',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist/server'),
+    filename: 'server.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: { presets: ['react', 'env'] },
+      },
+    ],
+  },
+};
+
+module.exports = [browserConfig, serverConfig];
